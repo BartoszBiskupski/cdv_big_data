3bac39e5475d96993dd4fa16286d48cd87bf2315
bartoszbiskupski <bart.biskupski@gmail.com>
2025-01-10 20:54:24 +0100

    Refactor dim_1.py to improve code readability and performance

--- big_data/workflows/spark/curated/dim_1.py
+++ big_data/workflows/spark/curated/dim_1.py
@@ -15,13 +15,13 @@ def transform(ec):
                     join(df_variable_section_position.alias("sec"), join_cond, "left")
                     .filter(F.col("sec.id_wymiar").isNotNull())
                     .select(
-                        F.concat(F.col("var.id_wymiar_1"), F.col("var.id_pozycja_1")).alias("id_dim_1"),
                         F.col("var.id_wymiar_1").alias("id_wymiar"),
                         F.col("var.id_pozycja_1").alias("id_pozycja"),
                         F.col("sec.nazwa_wymiar").alias("nazwa_wymiar"),
                         F.col("sec.nazwa_pozycja").alias("nazwa_pozycja"),
                     )
-                    )
+                    ).distinct()
+    df_transform = df_transform.withColumn("id_dim_1", F.concat(F.col("id_wymiar"), F.col("id_pozycja")))
     
     ec.update_config("df_transform", df_transform)
     print(f"Added df_transform to the config")
