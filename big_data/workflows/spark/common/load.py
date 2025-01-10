1462bed5e383584ff382b7952fd2a52a92757866
bartoszbiskupski <bart.biskupski@gmail.com>
2025-01-10 11:52:21 +0100

    Refactor load.py to improve performance by coalescing DataFrame before saving as table

--- big_data/workflows/spark/common/load.py
+++ big_data/workflows/spark/common/load.py
@@ -92,8 +92,8 @@ class Load_DataFrame:
         print(f"CREATE SCHEMA IF NOT EXISTS {self.catalog}.{self.source_name} MANAGED LOCATION `{self.base_uri}`")
         self.spark.sql(f"CREATE SCHEMA IF NOT EXISTS {self.catalog}.{self.source_name} MANAGED LOCATION '{self.base_uri}'")
         # Write DataFrame to ADLS
+        df_final = df_final.coalesce(1)
         (df_final.write
-            .coalesce(1)
             .saveAsTable(self.full_table_name, **writer_kwargs)
         )
         print(f"Table {self.full_table_name} saved to {self.full_uri}")
