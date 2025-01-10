3171ff2a470e1c41b4019f33b93c4a010ecd5543
bartoszbiskupski <bart.biskupski@gmail.com>
2025-01-10 21:32:52 +0100

    Refactor curated dim_1.json to use correct source and callable names

---
+++ big_data/workflows/spark/curated/dim_2.py
@@ -0,0 +1,29 @@
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+from big_data.workflows.spark.common.utils.config_loader import ExecutionContext
+
+spark = SparkSession.builder.getOrCreate()
+
+def transform(ec):
+    
+    df_variables = ec.config["data"]["dbw_variables_extract"]
+    df_variable_section_position = ec.config["data"]["dbw_variable_section_position"]
+    
+    join_cond = [
+        F.col("var.id_wymiar_2") == F.col("sec.id_wymiar"),
+        F.col("var.id_pozycja_2") == F.col("sec.id_pozycja"),
+    ]
+
+    df_transform = (df_variables.alias("var")
+                    .join(df_variable_section_position.alias("sec"), join_cond, "left")
+                    .filter(F.col("sec.id_wymiar").isNotNull())
+                    .select(
+                        F.col("var.id_wymiar_2").alias("id_wymiar"),
+                        F.col("var.id_pozycja_2").alias("id_pozycja"),
+                        F.col("sec.nazwa_wymiar").alias("nazwa_wymiar"),
+                        F.col("sec.nazwa_pozycja").alias("nazwa_pozycja"),
+                    )
+                    ).distinct()
+    df_transform = df_transform.withColumn("id_dim_2", F.concat(F.col("id_wymiar"), F.col("id_pozycja")))
+    ec.update_config("df_transform", df_transform)
+    print(f"Added df_transform to the config")
